8dec2f85a58cdad0df74344e3eb4b3f25baea35e
Jonathan Meldrum <meldrum.jon+git@gmail.com>
2019-06-25 13:38:55 -0600

    OLED Test

--- lib/Adafruit-SSD1351/examples/test/test.ino
+++ lib/Adafruit-SSD1351/examples/test/test.ino
@@ -27,6 +27,13 @@
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
 
+// You can use any (4 or) 5 pins 
+#define SCLK_PIN 2
+#define MOSI_PIN 3
+#define DC_PIN   4
+#define CS_PIN   5
+#define RST_PIN  6
+
 // Color definitions
 #define	BLACK           0x0000
 #define	BLUE            0x001F
@@ -42,15 +49,90 @@
 #include <SPI.h>
 
 // Option 1: use any pins but a little slower
-// Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);  
+Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);  
 
 // Option 2: must use the hardware SPI pins 
 // (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
 // an output. This is much faster - also required if you want
 // to use the microSD card (see the image drawing example)
-Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, 10, 9, 8);
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
 
 float p = 3.1415926;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("hello!");
+  tft.begin();
+
+  Serial.println("init");
+
+  // You can optionally rotate the display by running the line below.
+  // Note that a value of 0 means no rotation, 1 means 90 clockwise,
+  // 2 means 180 degrees clockwise, and 3 means 270 degrees clockwise.
+  //tft.setRotation(1);
+  // NOTE: The test pattern at the start will NOT be rotated!  The code
+  // for rendering the test pattern talks directly to the display and
+  // ignores any rotation.
+
+  uint16_t time = millis();
+  tft.fillRect(0, 0, 128, 128, BLACK);
+  time = millis() - time;
+  
+  Serial.println(time, DEC);
+  delay(500);
+  
+  lcdTestPattern();
+  delay(500);
+  
+  tft.invert(true);
+  delay(100);
+  tft.invert(false);
+  delay(100);
+
+  tft.fillScreen(BLACK);
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", WHITE);
+  delay(500);
+
+  // tft print function!
+  tftPrintTest();
+  delay(500);
+  
+  //a single pixel
+  tft.drawPixel(tft.width()/2, tft.height()/2, GREEN);
+  delay(500);
+
+  // line draw test
+  testlines(YELLOW);
+  delay(500);    
+ 
+  // optimized lines
+  testfastlines(RED, BLUE);
+  delay(500);    
+
+
+  testdrawrects(GREEN);
+  delay(1000);
+
+  testfillrects(YELLOW, MAGENTA);
+  delay(1000);
+
+  tft.fillScreen(BLACK);
+  testfillcircles(10, BLUE);
+  testdrawcircles(10, WHITE);
+  delay(1000);
+   
+  testroundrects();
+  delay(500);
+  
+  testtriangles();
+  delay(500);
+  
+  Serial.println("done");
+  delay(1000);
+}
+
+void loop() {
+}
 
 void testlines(uint16_t color) {
    tft.fillScreen(BLACK);
@@ -240,81 +322,4 @@ void lcdTestPattern(void)
     tft.fillRect(0, tft.height() * c / 8, tft.width(), tft.height() / 8,
       pgm_read_word(&colors[c]));
   }
-}
-
-void setup(void) {
-  Serial.begin(9600);
-  Serial.print("hello!");
-  tft.begin();
-
-  Serial.println("init");
-
-  // You can optionally rotate the display by running the line below.
-  // Note that a value of 0 means no rotation, 1 means 90 clockwise,
-  // 2 means 180 degrees clockwise, and 3 means 270 degrees clockwise.
-  //tft.setRotation(1);
-  // NOTE: The test pattern at the start will NOT be rotated!  The code
-  // for rendering the test pattern talks directly to the display and
-  // ignores any rotation.
-
-while(1) {
-  uint16_t time = millis();
-  tft.fillRect(0, 0, 128, 128, BLACK);
-  time = millis() - time;
-  
-  Serial.println(time, DEC);
-  delay(500);
-  
-  lcdTestPattern();
-  delay(500);
-  
-  tft.invert(true);
-  delay(100);
-  tft.invert(false);
-  delay(100);
-
-  tft.fillScreen(BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", WHITE);
-  delay(500);
-
-  // tft print function!
-  tftPrintTest();
-  delay(500);
-  
-  //a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, GREEN);
-  delay(500);
-
-  // line draw test
-  testlines(YELLOW);
-  delay(500);    
- 
-  // optimized lines
-  testfastlines(RED, BLUE);
-  delay(500);    
-
-
-  testdrawrects(GREEN);
-  delay(1000);
-
-  testfillrects(YELLOW, MAGENTA);
-  delay(1000);
-
-  tft.fillScreen(BLACK);
-  testfillcircles(10, BLUE);
-  testdrawcircles(10, WHITE);
-  delay(1000);
-   
-  testroundrects();
-  delay(500);
-  
-  testtriangles();
-  delay(500);
-  
-  Serial.println("done");
-  delay(1000);
-  }
-}
-
-void loop() {
 }
